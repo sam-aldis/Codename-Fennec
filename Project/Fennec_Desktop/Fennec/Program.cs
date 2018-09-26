600b3a4f7086c01b1de1d6ca7bb6c1a04716de83
Sam Aldis <samaldis@gmail.com>
2018-09-26 12:08:26 +0100

    Revert "Prune Dcos"
    This reverts commit 04171cc8571b20564ba39b8434e86c5855bb78ef.

---
+++ Project/Fennec_Desktop/Fennec/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gtk;
+
+namespace Fennec
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            // Check here if the user already has a token
+            //
+            MainWindow win = new MainWindow();
+            win.Show();
+            // Call the onload public function to load images etc.
+            win.OnLoad();
+            Application.Run();
+        }
+    }
+}
